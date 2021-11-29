c6ab55ea5d51e49b23f5e8937e3df67d61bc9764
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2021-11-29 20:35:01 +0100

    e

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import React, {useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { gsap } from "gsap";
-import './App.css'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./App.css";
 
-
+gsap. registerPlugin(ScrollTrigger)
+const sections = [
+  { title: "Title 1", subtitle: "Subtitle 1" },
+  { title: "Title 2", subtitle: "Subtitle 2" },
+  { title: "Title 3", subtitle: "Subtitle 3" }
+];
 function App() {
+  const [background, setBackground] = useState("#262645");
   const headerRef = useRef(null);
+  const revealRefs = useRef([]);
+  revealRefs.current = [];
+  const toggleBackground = () => {
+    const color = background !== "#262645" ? "#5A7D95" : "#1B4943";
+    setBackground(color);
+  };
+  useEffect(() => {
+    gsap.to(headerRef.current, {
+      duration: 1,
+      backgroundColor: background,
+      ease: "none"
+    });
+  }, [background]);
+
   useEffect(() => {
     gsap.from(headerRef.current, {
-        duration: 2,
-        autoAlpha: 0,
-        ease: "none",
-        delay: 1
-      });
+      duration: 2,
+      autoAlpha: 0,
+      ease: "none",
+      delay: 1
+    });
+    revealRefs.current.forEach((el, index) => {
+      gsap.fromTo(
+        el,
+        { autoAlpha: 0 },
+        {
+          duration: 1,
+          autoAlpha: 1,
+          ease: "none",
+          scrollTrigger: {
+            id: `section-${index+1}`,
+            trigger: el,
+            start: 'top center+=100',
+            toggleActions: 'play none none reverse',
+            markers: true
+          }
+        }
+      );
+    });
   }, []);
+
+  const addToRefs = el => {
+    if (el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el);
+    }
+    console.log(revealRefs.current);
+  };
   return (
     <div className="App">
       <header ref={headerRef} className="App-header">
+        <button onClick={() => toggleBackground()}>toggle</button>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. In voluptas
           explicabo, ipsum laboriosam mollitia repellat neque numquam tenetur
@@ -26,9 +73,14 @@ function App() {
         </p>
       </header>
       <main className="App-main">
-        <div className="App-section"></div>
-        <h2>Title</h2>
-        <p>Subtitle</p>
+        {sections.map(({ title, subtitle }) => {
+          return (
+            <div key={title} className="App-section" ref={addToRefs}>
+              <h2>{title}</h2>
+              <p>{subtitle}</p>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
